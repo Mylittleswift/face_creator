10aab205a7f382c36a119ee71c0d2cf930435790
Joey Li <mylittleswift@gmail.com>
2017-11-26 20:04:43 -0800

    first commit, v1.0

---
+++ Face Creator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Face Creator
+//
+//  Created by Joey Li on 1/1/18.
+//  Copyright © 2018 Joey Li. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
